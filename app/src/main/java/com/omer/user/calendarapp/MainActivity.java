daf0a4e0dcf2b1e821ad25b1330e7bc5ad486068
kurular4 <kurular4@gmail.com>
2018-08-06 23:37:37 +0300

    database connection and list, delete functions

--- app/src/main/java/com/omer/user/calendarapp/MainActivity.java
+++ app/src/main/java/com/omer/user/calendarapp/MainActivity.java
@@ -5,27 +5,34 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
+import com.omer.user.calendarapp.RestApi.ManagerAll;
+import com.onesignal.OneSignal;
+
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     CalendarView calendar;
-    ListView event;
-    ArrayList<Event> events;
-    EventsAdapter eventsAdapter;
-    Button addEvent, help;
+    ListView appointment;
+    List<Appointment> appointments;
+    AppointmentAdapter appointmentAdapter;
+    Button addEvent, help, searchb;
+    EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +40,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
         initialize();
-        listEventsToDate();
-        addEvent();
         help();
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, final int i, final int i1, final int i2) {
+                int i_m = i1 + 1;
+                Log.i("date", i2 + "/" + i_m + "/" + i);
+                listAppointments(i2 + "/" + i_m + "/" + i);
+                addEvent.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        LayoutInflater layoutInflater = getLayoutInflater();
+                        view = layoutInflater.inflate(R.layout.add_event, null);
+
+                        Button add = view.findViewById(R.id.add);
+                        Button cancel = view.findViewById(R.id.cancel);
+
+                        final String title = view.findViewById(R.id.title).toString();
+                        final String description = view.findViewById(R.id.description).toString();
+                        TextView date = view.findViewById(R.id.date);
+
+                        date.setText(i2+"/"+i1+"/"+i);
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setView(view);
+                        builder.setCancelable(true);
+
+                        final AlertDialog dialog = builder.create();
+
+                        dialog.show();
+
+                        add.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                //query(title, description, i2+"/"+i1+"/"+i")
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(), "Appointment created", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        cancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                dialog.cancel();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        searchb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     private void initialize() {
         calendar = findViewById(R.id.calendar);
-        event = findViewById(R.id.events);
-        events = new ArrayList<>();
+        appointment = findViewById(R.id.appointments);
+        appointments = new ArrayList<>();
         addEvent = findViewById(R.id.addevent);
         help = findViewById(R.id.help);
-    }
-
-    private void listEventsToDate() {
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                // query(i2,i1,i)
-                // it returns a list of events for selected date
-                // events = result;
-                eventsAdapter = new EventsAdapter(events, getApplicationContext(), MainActivity.this);
-                event.setAdapter(eventsAdapter);
-            }
-        });
+        searchb = findViewById(R.id.searchb);
+        search = findViewById(R.id.search);
     }
 
     private void help() {
@@ -87,54 +141,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void addEvent() {
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+    private void fillList() {
+        appointmentAdapter = new AppointmentAdapter(appointments, getApplicationContext(), MainActivity.this);
+        appointment.setAdapter(appointmentAdapter);
+    }
+
+    private void listAppointments(String date){
+        Call<List<Appointment>> x = ManagerAll.getInstance().all_appointments(date);
+        x.enqueue(new Callback<List<Appointment>>() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, final int i, final int i1, final int i2) {
-                addEvent.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        LayoutInflater layoutInflater = getLayoutInflater();
-                        view = layoutInflater.inflate(R.layout.add_event, null);
+            public void onResponse(Call<List<Appointment>> call, Response<List<Appointment>> response) {
+                if(response.isSuccessful()) {
+                    appointments = response.body();
+                    Log.i("date", appointments.toString() + " exists");
+                    fillList();
+                }
+            }
 
-                        Button add = view.findViewById(R.id.add);
-                        Button cancel = view.findViewById(R.id.cancel);
-
-                        final String title = view.findViewById(R.id.title).toString();
-                        final String description = view.findViewById(R.id.description).toString();
-                        TextView date = view.findViewById(R.id.date);
-
-                        date.setText(i2+"/"+i1+"/"+i);
-
-
-
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setView(view);
-                        builder.setCancelable(true);
-
-                        final AlertDialog dialog = builder.create();
-
-                        dialog.show();
-
-                        add.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                //query(title, description, i2+"/"+i1+"/"+i")
-                                dialog.cancel();
-                                Toast.makeText(getApplicationContext(), "Appointment created", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                        cancel.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                dialog.cancel();
-                            }
-                        });
-                    }
-                });
+            @Override
+            public void onFailure(Call<List<Appointment>> call, Throwable t) {
+                Log.i("date", t.getMessage().toString());
             }
         });
     }
+
 
 }
---
+++ app/src/main/java/com/omer/user/calendarapp/RestApi/ManagerAll.java
@@ -0,0 +1,36 @@
+package com.omer.user.calendarapp.RestApi;
+
+import com.omer.user.calendarapp.Appointment;
+
+import java.util.List;
+
+import retrofit2.Call;
+
+public class ManagerAll extends BaseManager {
+
+    private static ManagerAll ourInstance = new ManagerAll();
+
+    public static synchronized ManagerAll getInstance() {
+        return ourInstance;
+    }
+
+    public Call<Appointment> add_appointment(String title, String description, String date) {
+        Call<Appointment> call = getRestApiClient().add_appointment(title, description, date);
+        return call;
+    }
+
+    public Call<Appointment> delete_appointment(int id) {
+        Call<Appointment> call = getRestApiClient().delete_appointment(id);
+        return call;
+    }
+
+    public Call<Appointment> update_appointment(int id, String title, String description) {
+        Call<Appointment> call = getRestApiClient().update_appointment(id, title, description);
+        return call;
+    }
+
+    public Call<List<Appointment>> all_appointments(String date) {
+        Call<List<Appointment>> call = getRestApiClient().all_appointments(date);
+        return call;
+    }
+}
